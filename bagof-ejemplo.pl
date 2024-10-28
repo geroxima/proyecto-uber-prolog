50282d1c00aee6a5c1072c012abe27f835e5b5f3
geroxima <geroshima123@gmail.com>
2024-10-28 10:33:39 -0300

    refactor code to use custom member predicate

--- bagof-ejemplo.pl
+++ bagof-ejemplo.pl
@@ -7,13 +7,19 @@ ruta(c, d, 4, 10).
 ruta(c, e, 6, 12).
 ruta(d, e, 2, 5).
 
-% Rutas inversas
-ruta(b, a, 5, 10).
-ruta(c, a, 7, 15).
-ruta(d, b, 3, 7).
-ruta(d, c, 4, 10).
-ruta(e, c, 6, 12).
-ruta(e, d, 2, 5).
+% % Rutas inversas
+% ruta(b, a, 5, 10).
+% ruta(c, a, 7, 15).
+% ruta(d, b, 3, 7).
+% ruta(d, c, 4, 10).
+% ruta(e, c, 6, 12).
+% ruta(e, d, 2, 5).
+
+
+% mi_mi_member(Elemento, Lista)
+mi_member(X, [X|_]).
+mi_member(X, [_|T]) :-
+    mi_member(X, T).
 
 % camino(UbicacionInicial, UbicacionFinal, Camino, DistanciaTotal, TiempoTotal)
 camino(Inicio, Fin, [Inicio|Ruta], DistanciaTotal, TiempoTotal) :-
@@ -24,7 +30,7 @@ camino_aux(Ubicacion, Ubicacion, _, [], Distancia, Tiempo, Distancia, Tiempo).
 
 camino_aux(Inicio, Fin, Visitados, [Siguiente|Ruta], DistanciaAcum, TiempoAcum, DistanciaTotal, TiempoTotal) :-
     ruta(Inicio, Siguiente, DistanciaPaso, TiempoPaso),
-    \+ member(Siguiente, Visitados),  % evita ciclos
+    \+ mi_member(Siguiente, Visitados),  % evita ciclos
     NuevaDistancia is DistanciaAcum + DistanciaPaso,
     NuevoTiempo is TiempoAcum + TiempoPaso,
     camino_aux(Siguiente, Fin, [Siguiente|Visitados], Ruta, NuevaDistancia, NuevoTiempo, DistanciaTotal, TiempoTotal).
@@ -37,6 +43,9 @@ ruta_mas_corta(Inicio, Fin, Camino, Distancia, Tiempo) :-
 % min_distancia(ListaDeRutas, RutaMinima)
 min_distancia([Ruta], Ruta).
 min_distancia([(C1, D1, T1), (C2, D2, T2) | Rutas], MinRuta) :-
-    (D1 =< D2 -> min_distancia([(C1, D1, T1) | Rutas], MinRuta)
-    ; min_distancia([(C2, D2, T2) | Rutas], MinRuta)).
+    D1 =< D2,
+    min_distancia([(C1, D1, T1) | Rutas], MinRuta).
+min_distancia([(C1, D1, T1), (C2, D2, T2) | Rutas], MinRuta) :-
+    D1 > D2,
+    min_distancia([(C2, D2, T2) | Rutas], MinRuta).
 
