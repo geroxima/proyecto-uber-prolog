1e2bc947eac5274fe1bb52b889b60f6c63be6896
geroxima <geroshima123@gmail.com>
2024-11-04 01:05:17 -0300

    add semaforo counting, pricing rides, fix recursive probloem limiting ride max distance, fix map direcion, add stoplights facts in map

--- bagof-ejemplo.pl
+++ bagof-ejemplo.pl
@@ -1,42 +1,136 @@
 :- set_prolog_flag(toplevel_print_options, [quoted(true), portray(true), max_depth(0), max_length(0)]).
 :- set_prolog_flag(answer_write_options, [max_depth(0)]).
 
-:- consult('mapa.pl').
+:- consult('mapa-sin-hora.pl').
 
-% ruta(nodoA, nodoB, distancia, transitoDe0A6, transito de 7 a 12, transito de 13 a 18, transito de 19 a 00)
-
-
-
-% mi_mi_member(Elemento, Lista)
+% mi_member(Elemento, Lista)
 mi_member(X, [X|_]).
 mi_member(X, [_|T]) :-
     mi_member(X, T).
 
-% camino(UbicacionInicial, UbicacionFinal, Camino, DistanciaTotal, TiempoTotal)
-camino(Inicio, Fin, [Inicio|Ruta], DistanciaTotal, TiempoTotal) :-
-    camino_aux(Inicio, Fin, [Inicio], Ruta, 0, 0, DistanciaTotal, TiempoTotal).
+% sublist(Lista, SubLista, Inicio, Longitud)
+sublist(Lista, SubLista, Inicio, Longitud) :-
+    longitud(Prefix, Inicio),
+    concatenar(Prefix, Rest, Lista),
+    longitud(SubLista, Longitud),
+    concatenar(SubLista, _, Rest).
 
-% camino_aux es una regla auxiliar que acumula la distancia y el tiempo
-camino_aux(Ubicacion, Ubicacion, _, [], Distancia, Tiempo, Distancia, Tiempo).
+% count_semaforos(Camino, Cantidad)
+% Caso base: cuando la lista está vacía, la cantidad de semáforos es 0.
+count_semaforos([], 0).
 
-camino_aux(Inicio, Fin, Visitados, [Siguiente|Ruta], DistanciaAcum, TiempoAcum, DistanciaTotal, TiempoTotal) :-
+% Caso cuando el primer nodo es un semáforo:
+count_semaforos([Nodo|Resto], Cantidad) :-
+    semaforo(Nodo),            % Verifica si el nodo es un semáforo
+    count_semaforos(Resto, CantidadResto),
+    Cantidad is CantidadResto + 1.
+
+% Caso cuando el primer nodo no es un semáforo:
+count_semaforos([Nodo|Resto], Cantidad) :-
+    \+ semaforo(Nodo),         % Verifica que el nodo no es un semáforo
+    count_semaforos(Resto, Cantidad).
+% Definir tarifa fija por kilómetro
+tarifa_por_km(10). 
+
+
+% camino(UbicacionInicial, UbicacionFinal, Camino, DistanciaTotal, TiempoTotal, SemaforosTotal, ListaSemaforos, Precio)
+camino(Inicio, Fin, [Inicio|Ruta], DistanciaTotal, TiempoTotal, SemaforosTotal, ListaSemaforos, Precio) :-
+    camino_aux(Inicio, Fin, [Inicio], Ruta, 0, 0, 0, DistanciaTotal, TiempoTotal, SemaforosTotal, [], ListaSemaforos),
+    tarifa_por_km(Tarifa),
+    Precio is DistanciaTotal * Tarifa.
+
+% camino_aux es una regla auxiliar que acumula la distancia, el tiempo y los semaforos
+camino_aux(Ubicacion, Ubicacion, _, [], Distancia, Tiempo, Semaforos, Distancia, Tiempo, Semaforos, ListaSemaforos, ListaSemaforos).
+
+camino_aux(Inicio, Fin, Visitados, [Siguiente|Ruta], DistanciaAcum, TiempoAcum, SemaforosAcum, DistanciaTotal, TiempoTotal, SemaforosTotal, ListaSemaforosAcum, ListaSemaforos) :-
     ruta(Inicio, Siguiente, DistanciaPaso, TiempoPaso),
-    \+ mi_member(Siguiente, Visitados),  % evita ciclos
+    \+ mi_member(Siguiente, Visitados),  % Evita ciclos
     NuevaDistancia is DistanciaAcum + DistanciaPaso,
-    NuevoTiempo is TiempoAcum + TiempoPaso,
-    camino_aux(Siguiente, Fin, [Siguiente|Visitados], Ruta, NuevaDistancia, NuevoTiempo, DistanciaTotal, TiempoTotal).
+    NuevaDistancia =< 2000,  % Verifica si la distancia excede 2000
+    % Caso cuando Siguiente es un semáforo
+    semaforo(Siguiente),
+    NuevoSemaforos is SemaforosAcum + 1,
+    append(ListaSemaforosAcum, [Siguiente], NuevaListaSemaforos),
+    TiempoExtra is 1,
+    NuevoTiempo is TiempoAcum + TiempoPaso + TiempoExtra,
+    camino_aux(Siguiente, Fin, [Siguiente|Visitados], Ruta, NuevaDistancia, NuevoTiempo, NuevoSemaforos, DistanciaTotal, TiempoTotal, SemaforosTotal, NuevaListaSemaforos, ListaSemaforos).
 
-% ruta_mas_corta(UbicacionInicial, UbicacionFinal, Camino, Distancia, Tiempo)
-ruta_mas_corta(Inicio, Fin, Camino, Distancia, Tiempo) :-
-    bagof((C, D, T), camino(Inicio, Fin, C, D, T), Rutas),
-    min_distancia(Rutas, (Camino, Distancia, Tiempo)).
+camino_aux(Inicio, Fin, Visitados, [Siguiente|Ruta], DistanciaAcum, TiempoAcum, SemaforosAcum, DistanciaTotal, TiempoTotal, SemaforosTotal, ListaSemaforosAcum, ListaSemaforos) :-
+    ruta(Inicio, Siguiente, DistanciaPaso, TiempoPaso),
+    \+ mi_member(Siguiente, Visitados),  % Evita ciclos
+    NuevaDistancia is DistanciaAcum + DistanciaPaso,
+    NuevaDistancia =< 2000,  % Verifica si la distancia excede 2000
+    % Caso cuando Siguiente no es un semáforo
+    \+ semaforo(Siguiente),
+    NuevoSemaforos is SemaforosAcum,
+    NuevaListaSemaforos = ListaSemaforosAcum,
+    TiempoExtra is 0,
+    NuevoTiempo is TiempoAcum + TiempoPaso + TiempoExtra,
+    camino_aux(Siguiente, Fin, [Siguiente|Visitados], Ruta, NuevaDistancia, NuevoTiempo, NuevoSemaforos, DistanciaTotal, TiempoTotal, SemaforosTotal, NuevaListaSemaforos, ListaSemaforos).
+
+% ruta_mas_corta(UbicacionInicial, UbicacionFinal, Camino, Distancia, Tiempo, Precio)
+ruta_mas_corta(Inicio, Fin, Camino, Distancia, Tiempo, Precio) :-
+    bagof((C, D, T, S, L, P), camino(Inicio, Fin, C, D, T, S, L, P), Rutas),
+    min_distancia(Rutas, (Camino, Distancia, Tiempo, _, _, Precio)).
+
+% ruta_menos_semaforos(UbicacionInicial, UbicacionFinal, Camino, Distancia, Tiempo, Semaforos, ListaSemaforos, Precio)
+ruta_menos_semaforos(Inicio, Fin, Camino, Distancia, Tiempo, Semaforos, ListaSemaforos, Precio) :-
+    bagof((C, D, T, S, L, P), camino(Inicio, Fin, C, D, T, S, L, P), Rutas),
+    min_semaforos(Rutas, (Camino, Distancia, Tiempo, Semaforos, ListaSemaforos, Precio)).
 
 % min_distancia(ListaDeRutas, RutaMinima)
 min_distancia([Ruta], Ruta).
-min_distancia([(_, D1, _), (_, D2, _) | Rutas], MinRuta) :-
+min_distancia([(C1, D1, T1, S1, L1, P1), (C2, D2, T2, S2, L2, P2) | Rutas], MinRuta) :-
     D1 =< D2,
-    min_distancia([(_, D1, _) | Rutas], MinRuta).
-min_distancia([(_, D1, _), (C2, D2, T2) | Rutas], MinRuta) :-
+    min_distancia([(C1, D1, T1, S1, L1, P1) | Rutas], MinRuta).
+min_distancia([(C1, D1, T1, S1, L1, P1), (C2, D2, T2, S2, L2, P2) | Rutas], MinRuta) :-
     D1 > D2,
-    min_distancia([(C2, D2, T2) | Rutas], MinRuta).
+    min_distancia([(C2, D2, T2, S2, L2, P2) | Rutas], MinRuta).
+
+% min_semaforos(ListaDeRutas, RutaMinima)
+min_semaforos([Ruta], Ruta).
+min_semaforos([(C1, D1, T1, S1, L1, P1), (C2, D2, T2, S2, L2, P2) | Rutas], MinRuta) :-
+    S1 =< S2,
+    min_semaforos([(C1, D1, T1, S1, L1, P1) | Rutas], MinRuta).
+min_semaforos([(C1, D1, T1, S1, L1, P1), (C2, D2, T2, S2, L2, P2) | Rutas], MinRuta) :-
+    S1 > S2,
+    min_semaforos([(C2, D2, T2, S2, L2, P2) | Rutas], MinRuta).
+
+% viaje(UbicacionInicial, UbicacionFinal, Rutas)
+viaje(Inicio, Fin, MejoresRutas) :-
+    bagof((C, D, T, S, L, P), camino(Inicio, Fin, C, D, T, S, L, P), Rutas),
+    sort(2, @=<, Rutas, RutasOrdenadas),  % Ordenar rutas por distancia
+    sublist(RutasOrdenadas, MejoresRutas, 0, 3).  % Seleccionar las tres primeras rutas
+
+
+% sublist(Lista, SubLista, Inicio, Longitud)
+% longitud(Lista, Longitud)
+longitud([], 0).
+longitud([_|T], N) :-
+    longitud(T, N1),
+    N is N1 + 1.
+
+% concatenar(Lista1, Lista2, Resultado)
+concatenar([], L, L).
+concatenar([H|T], L, [H|R]) :-
+    concatenar(T, L, R).
+
+
+% imprimir_rutas(ListaDeRutas)
+imprimir_rutas([]).
+imprimir_rutas([(Camino, Distancia, Tiempo, Semaforos, ListaSemaforos, Precio) | Rutas]) :-
+    format('------------~n'),
+    format('Camino: ~w~n', [Camino]),
+    format('Distancia: ~w~n', [Distancia]),
+    format('Tiempo: ~w~n', [Tiempo]),
+    format('Semaforos: ~w~n', [Semaforos]),
+    format('Intersecciones con semaforos: ~w~n', [ListaSemaforos]),
+    format('Precio: ~2f Gs~n', [Precio]),
+    imprimir_rutas(Rutas).
+
+
+% viaje_imprimir(UbicacionInicial, UbicacionFinal)
+viaje_imprimir(Inicio, Fin) :-
+    viaje(Inicio, Fin, MejoresRutas),
+    imprimir_rutas(MejoresRutas).
 
---
+++ mapa-sin-hora.pl
@@ -0,0 +1,234 @@
+% ruta(inicio, fin, distancia, 00-6, 7-12, 13-18, 19-24).
+% Calles horizontales (de izquierda a derecha)
+ruta(a0, b0, 100, 2).
+ruta(b0, c0, 100, 2).
+ruta(c0, d0, 100, 2).
+ruta(d0, e0, 100, 3).
+ruta(e0, f0, 100, 3).
+ruta(f0, g0, 100, 3).
+ruta(g0, h0, 100, 3).
+ruta(h0, i0, 100, 2).
+ruta(i0, j0, 100, 2).
+
+ruta(b0, a0, 100, 2).
+ruta(c0, b0, 100, 2).
+ruta(d0, c0, 100, 2).
+ruta(e0, d0, 100, 3).
+ruta(f0, e0, 100, 3).
+ruta(g0, f0, 100, 3).
+ruta(h0, g0, 100, 3).
+ruta(i0, h0, 100, 2).
+ruta(j0, i0, 100, 2).
+
+ruta(b1, a1, 100, 2).
+ruta(c1, b1, 100, 2).
+ruta(d1, c1, 100, 2).
+ruta(e1, d1, 100, 3).
+ruta(f1, e1, 100, 3).
+ruta(g1, f1, 100, 3).
+ruta(h1, g1, 100, 3).
+ruta(i1, h1, 100, 2).
+ruta(j1, i1, 100, 2).
+
+ruta(a2, b2, 100, 2).
+ruta(b2, c2, 100, 2).
+ruta(c2, d2, 100, 2).
+ruta(d2, e2, 100, 3).
+ruta(e2, f2, 100, 3).
+ruta(f2, g2, 100, 3).
+ruta(g2, h2, 100, 3).
+ruta(h2, i2, 100, 2).
+ruta(i2, j2, 100, 2).
+
+ruta(b3, a3, 100, 2).
+ruta(c3, b3, 100, 2).
+ruta(d3, c3, 100, 2).
+ruta(e3, d3, 100, 3).
+ruta(f3, e3, 100, 3).
+ruta(g3, f3, 100, 3).
+ruta(h3, g3, 100, 3).
+ruta(i3, h3, 100, 2).
+ruta(j3, i3, 100, 2).
+
+ruta(a4, b4, 100, 2).
+ruta(b4, c4, 100, 2).
+ruta(c4, d4, 100, 2).
+ruta(d4, e4, 100, 3).
+ruta(e4, f4, 100, 3).
+ruta(f4, g4, 100, 3).
+ruta(g4, h4, 100, 3).
+ruta(h4, i4, 100, 2).
+ruta(i4, j4, 100, 2).
+
+ruta(b5, a5, 100, 2).
+ruta(c5, b5, 100, 2).
+ruta(d5, c5, 100, 2).
+ruta(e5, d5, 100, 3).
+ruta(f5, e5, 100, 3).
+ruta(g5, f5, 100, 3).
+ruta(h5, g5, 100, 3).
+ruta(i5, h5, 100, 2).
+ruta(j5, i5, 100, 2).
+
+ruta(a6, b6, 100, 2).
+ruta(b6, c6, 100, 2).
+ruta(c6, d6, 100, 2).
+ruta(d6, e6, 100, 3).
+ruta(e6, f6, 100, 3).
+ruta(f6, g6, 100, 3).
+ruta(g6, h6, 100, 3).
+ruta(h6, i6, 100, 2).
+ruta(i6, j6, 100, 2).
+
+ruta(b7, a7, 100, 2).
+ruta(c7, b7, 100, 2).
+ruta(d7, c7, 100, 2).
+ruta(e7, d7, 100, 3).
+ruta(f7, e7, 100, 3).
+ruta(g7, f7, 100, 3).
+ruta(h7, g7, 100, 3).
+ruta(i7, h7, 100, 2).
+ruta(j7, i7, 100, 2).
+
+ruta(a8, b8, 100, 2).
+ruta(b8, c8, 100, 2).
+ruta(c8, d8, 100, 2).
+ruta(d8, e8, 100, 3).
+ruta(e8, f8, 100, 3).
+ruta(f8, g8, 100, 3).
+ruta(g8, h8, 100, 3).
+ruta(h8, i8, 100, 2).
+ruta(i8, j8, 100, 2).
+
+% Calles verticales
+ruta(a0, a1, 100, 2).
+ruta(a1, a2, 100, 2).
+ruta(a2, a3, 100, 2).
+ruta(a3, a4, 100, 2).
+ruta(a4, a5, 100, 3).
+ruta(a5, a6, 100, 3).
+ruta(a6, a7, 100, 3).
+ruta(a7, a8, 100, 3).
+
+ruta(a1, a0, 100, 2).
+ruta(a2, a1, 100, 2).
+ruta(a3, a2, 100, 2).
+ruta(a4, a3, 100, 2).
+ruta(a5, a4, 100, 3).
+ruta(a6, a5, 100, 3).
+ruta(a7, a6, 100, 3).
+ruta(a8, a7, 100, 3).
+
+ruta(b0, b1, 100, 2).
+ruta(b1, b2, 100, 2).
+ruta(b2, b3, 100, 2).
+ruta(b3, b4, 100, 2).
+ruta(b4, b5, 100, 3).
+ruta(b5, b6, 100, 3).
+ruta(b6, b7, 100, 3).
+ruta(b7, b8, 100, 3).
+
+ruta(c1, c0, 100, 2).
+ruta(c2, c1, 100, 2).
+ruta(c3, c2, 100, 2).
+ruta(c4, c3, 100, 2).
+ruta(c5, c4, 100, 3).
+ruta(c6, c5, 100, 3).
+ruta(c7, c6, 100, 3).
+ruta(c8, c7, 100, 3).
+
+ruta(d0, d1, 100, 2).
+ruta(d1, d2, 100, 2).
+ruta(d2, d3, 100, 2).
+ruta(d3, d4, 100, 2).
+ruta(d4, d5, 100, 3).
+ruta(d5, d6, 100, 3).
+ruta(d6, d7, 100, 3).
+ruta(d7, d8, 100, 3).
+
+ruta(e1, e0, 100, 2).
+ruta(e2, e1, 100, 2).
+ruta(e3, e2, 100, 2).
+ruta(e4, e3, 100, 2).
+ruta(e5, e4, 100, 3).
+ruta(e6, e5, 100, 3).
+ruta(e7, e6, 100, 3).
+ruta(e8, e7, 100, 3).
+
+ruta(f0, f1, 100, 2).
+ruta(f1, f2, 100, 2).
+ruta(f2, f3, 100, 2).
+ruta(f3, f4, 100, 2).
+ruta(f4, f5, 100, 3).
+ruta(f5, f6, 100, 3).
+ruta(f6, f7, 100, 3).
+ruta(f7, f8, 100, 3).
+
+ruta(g1, g0, 100, 2).
+ruta(g2, g1, 100, 2).
+ruta(g3, g2, 100, 2).
+ruta(g4, g3, 100, 2).
+ruta(g5, g4, 100, 3).
+ruta(g6, g5, 100, 3).
+ruta(g7, g6, 100, 3).
+ruta(g8, g7, 100, 3).
+
+ruta(h0, h1, 100, 2).
+ruta(h1, h2, 100, 2).
+ruta(h2, h3, 100, 2).
+ruta(h3, h4, 100, 2).
+ruta(h4, h5, 100, 3).
+ruta(h5, h6, 100, 3).
+ruta(h6, h7, 100, 3).
+ruta(h7, h8, 100, 3).
+
+ruta(i1, i0, 100, 2).
+ruta(i2, i1, 100, 2).
+ruta(i3, i2, 100, 2).
+ruta(i4, i3, 100, 2).
+ruta(i5, i4, 100, 3).
+ruta(i6, i5, 100, 3).
+ruta(i7, i6, 100, 3).
+ruta(i8, i7, 100, 3).
+
+ruta(j0, j1, 100, 2).
+ruta(j1, j2, 100, 2).
+ruta(j2, j3, 100, 2).
+ruta(j3, j4, 100, 2).
+ruta(j4, j5, 100, 3).
+ruta(j5, j6, 100, 3).
+ruta(j6, j7, 100, 3).
+ruta(j7, j8, 100, 3).
+
+ruta(j1, j0, 100, 2).
+ruta(j2, j1, 100, 2).
+ruta(j3, j2, 100, 2).
+ruta(j4, j3, 100, 2).
+ruta(j5, j4, 100, 3).
+ruta(j6, j5, 100, 3).
+ruta(j7, j6, 100, 3).
+ruta(j8, j7, 100, 3).
+
+% Semaforos
+
+semaforo(a0).
+semaforo(c0).
+semaforo(d0).
+semaforo(e0).
+semaforo(f0).
+semaforo(g0).
+semaforo(h0).
+semaforo(i0).
+semaforo(j0).
+semaforo(c1).
+semaforo(e2).
+semaforo(e3).
+semaforo(e4).
+semaforo(e5).
+semaforo(e6).
+semaforo(e7).
+semaforo(d7).
+semaforo(d4).
+semaforo(f4).
+semaforo(j4).
+semaforo(j8).
