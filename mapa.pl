e1a0c7eb67f70e475a5287e33a568380422c2951
geroxima <geroshima123@gmail.com>
2024-11-05 12:59:37 -0300

    added places destinations, removed street names

--- mapa.pl
+++ mapa.pl
@@ -1,12 +1,33 @@
 % ruta(inicio, fin, distancia, 00-6, 7-12, 13-18, 19-24).
 % Calles horizontales (de izquierda a derecha)
-calle('Constitución Nacional', [a0, b0, c0, d0, e0, f0, g0, h0, i0 , j0]).
-calle('25 de mayo', [a1, b1, c1, d1, e1, f1, g1, h1, i1 , j1]).
-calle('Villarica', [a2, b2, c2, d2, e2, f2, g2, h2, i2 , j2]).
-calle('Tomás Romero Pereira', [a3, b3, c3, d3, e3, f3, g3, h3, i3 , j3]).
-calle('14 de mayo', [a4, b4, c4, d4, e4, f4, g4, h4, i4, j4]).
-calle('Cerro Corá', [a5, b5, c5, d5, e5, f5, g5, h5, i5, j5]).
-calle('Monseñor Wissen', [a6, b6, c6, d6, e6, f6, g6, h6, i6, j6]).
+% calle('Av. Caballero ←→ ', [a0, b0, c0, d0, e0, f0, g0, h0, i0 , j0]).
+% calle('Constitución Nacional ← ', [a1, b1, c1, d1, e1, f1, g1, h1, i1 , j1]).
+% calle('25 de mayo → ', [a2, b2, c2, d2, e2, f2, g2, h2, i2 , j2]).
+% calle('Villarica ← ', [a3, b3, c3, d3, e3, f3, g3, h3, i3 , j3]).
+% calle('Tomás Romero Pereira → ', [a4, b4, c4, d4, e4, f4, g4, h4, i4, j4]).
+% calle('14 de mayo ← ', [a5, b5, c5, d5, e5, f5, g5, h5, i5, j5]).
+% calle('Cerro Corá → ', [a6, b6, c6, d6, e6, f6, g6, h6, i6, j6]).
+% calle('Monseñor Wissen ← ', [a7, b7, c7, d7, e7, f7, g7, h7, i7, j7]).
+% calle('Curupayty → ', [a8, b8, c8, d8, e8, f8, g8, h8, i8, j8]).
+
+% calle('Av. Francia ↑↓ ', [a0, a1, a2, a3, a4, a5, a6, a7, a8]).
+% calle('Antequera ↓ ', [b0, b1, b2, b3, b4, b5, b6, b7, b8]).
+% calle('General Artigas ↑ ', [c0, c1, c2, c3, c4, c5, c6, c7, c8]).
+% calle('Juan L. Mallorquín ↓ ', [d0, d1, d2, d3, d4, d5, d6, d7, d8]).
+% calle('Mcal. Estigarribia ↑ ', [e0, e1, e2, e3, e4, e5, e6, e7, e8]).
+% calle('Carlos A. López ↓ ', [f0, f1, f2, f3, f4, f5, f6, f7, f8]).
+% calle('Lomas Valentinas ↑ ', [g0, g1, g2, g3, g4, g5, g6, g7, g8]).
+% calle('Independencia Nacional ↓ ', [h0, h1, h2, h3, h4, h5, h6, h7, h8]).
+% calle('Honorio Gonzáles ↑ ', [i0, i1, i2, i3, i4, i5, i6, i7, i8]).
+% calle('Av. Irrazábal ↑↓ ', [j0, j1, j2, j3, j4, j5, j6, j7, j8]).
+
+lugar('McDonalds', a0).
+lugar('Burger King', a1).
+lugar('Popeyes', a2).
+lugar('KFC', a3).
+lugar('Pizza Hut', a4).
+lugar('Hiroshima Restaurante', a5).
+lugar('Superseis', a6).
 
 
 ruta(a0, b0, 100, 1, 1, 1, 1).
